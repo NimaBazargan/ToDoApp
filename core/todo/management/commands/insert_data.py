1a5d3ecc0c5f8245767b52e61753e3502e674787
NimaBazargan <nima.kazemzadeh.bazargan@gmail.com>
2025-10-13 14:39:50 +0330

    reformat by black

--- core/todo/management/commands/insert_data.py
+++ core/todo/management/commands/insert_data.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
 import random
-from datetime import datetime
 from accounts.models import User, Profile
 from todo.models import Task
+
 
 class Command(BaseCommand):
     help = "inserting dummy data"
@@ -13,16 +13,16 @@ class Command(BaseCommand):
         self.fake = Faker()
 
     def handle(self, *args, **options):
-        user = User.objects.create_user(email=self.fake.email(),password='Test@123456')
+        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
         profile = Profile.objects.get(user=user)
         profile.first_name = self.fake.first_name()
         profile.last_name = self.fake.last_name()
         profile.description = self.fake.paragraph(nb_sentences=5)
-        profile.save() 
+        profile.save()
 
         for _ in range(5):
             Task.objects.create(
-                user = profile,
-                title = self.fake.sentence(nb_words=2)[:-1],
-                complete = random.choice([True,False])
+                user=profile,
+                title=self.fake.sentence(nb_words=2)[:-1],
+                complete=random.choice([True, False]),
             )
